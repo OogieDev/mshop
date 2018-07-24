8ed32fcc19fede9f01d11b26364d8295a9fadef3
Oogie <oogie.webdev@gmail.com>
2018-07-24 03:19:57 +0300

    LESSON7 Router part2. first Main and Page controllers

--- vendor/ishop/core/Router.php
+++ vendor/ishop/core/Router.php
@@ -34,20 +34,74 @@ class Router
      * возвращает текущий маршрут
      * @return array
      */
-    public static function getRoute(){
+    public static function getRoute()
+    {
         return self::$route;
     }
 
-    public static function dispatch($url){
+    public static function dispatch($url)
+    {
         if(self::matchRoute($url)){
-            echo 'OK';
+            $controller = 'app\controllers\\' . self::$route['prefix'] . self::$route['controller'] . 'Controller';
+            if(class_exists($controller)){
+                $controllerObject = new $controller(self::$route);
+                $action = self::lowerCamelCase(self::$route['action']) . 'Action';
+                if(method_exists($controllerObject, $action)){
+                    $controllerObject->$action();
+                }else{
+                    throw new \Exception("Метод $controller->$action не найден");
+                }
+            }else{
+                throw new \Exception("Контроллер $controller не найден", 404);
+            }
         }else{
-            echo 'No';
+            throw new \Exception("Страница не найдена", 404);
         }
     }
 
-    public static function matchRoute($url){
+    public static function matchRoute($url)
+    {
+        //проходимся по таблице маршрутов
+        foreach (self::$routes as $pattern => $route){
+            //если есть совпаления с регексом
+            if(preg_match("#{$pattern}#", $url, $matches)){
+                //получаем массив типа controller => page и view => add
+                //и записываем в переменную route под этими ключами эти значения
+                //записываем значения в контроллер и вьюшку
+                foreach ($matches as $key => $value){
+                    if(is_string($key)){
+                        $route[$key] = $value;
+                    }
+                }
+                if(empty($route['action'])){
+                    $route['action'] = 'index';
+                }
+                if(!isset($route['prefix'])){
+                    $route['prefix'] = '';
+                }
+                else{
+                    $route['prefix'] .= '\\';
+                }
+                $route['controller'] = self::upperCamelCase($route['controller']);
+                self::$route = $route;
+                return true;
+            }
+        }
         return false;
     }
+
+    //CamelCase
+    protected static function upperCamelCase($name)
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $name)));
+
+    }
+
+    //camelCase
+    protected static function lowerCamelCase($name)
+    {
+        return lcfirst(self::upperCamelCase($name));
+    }
+
 
 }
