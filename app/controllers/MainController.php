7204d3535107414ac5fe3c7abe322fd11b6db27e
Вадим <oogie.webdev@gmail.com>
2018-08-04 00:58:55 +0300

    part2 lesson1. add template

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,17 +8,8 @@ class MainController extends AppController
 {
 
     public function indexAction(){
-        $posts = \R::findAll('test');
         $this->setMeta('Title', 'description', 'keywords');
-        $name = 'john';
-        $age = '30';
-        $this->set(compact('age', 'name', 'posts'));
-        $cache = Cache::instance();
-        $data = $cache->get('test');
-        if(!$data){
-            $cache->set('test', $name);
-        }
-        debug($data);
+
     }
 
 }
